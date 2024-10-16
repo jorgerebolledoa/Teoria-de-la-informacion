2038902bb419d576ba5f64209baeb4039b5f23e9
jorgerebolledoa <aziel.az18@gmail.com>
2024-10-16 16:09:30 -0300

    clase5 comentado

--- Clase 5/2.r
+++ Clase 5/2.r
@@ -1,7 +1,16 @@
-Datos<-read.csv("past-rounded.csv", header = TRUE, sep = ",")
+# Leer el archivo CSV llamado "past-rounded.csv" con encabezados y separado por comas
+Datos <- read.csv("past-rounded.csv", header = TRUE, sep = ",")
+
+# Obtener los valores únicos de la columna 'O' del dataframe 'Datos', omitiendo los valores NA y ordenándolos en orden descendente
 U <- sort(x = unique(na.omit(Datos$O)), decreasing = TRUE)
-P<-c()
+
+# Inicializar un vector vacío para almacenar las frecuencias
+P <- c()
+
+# Calcular la frecuencia de cada valor único en 'U' y almacenarlo en 'P'
 for(i in 1:length(U)){
-  P[i]<- sum(Datos$O == U[i], na.rm = TRUE)
+    P[i] <- sum(Datos$O == U[i], na.rm = TRUE)
 }
-plot(U,P,log="x")
+
+# Graficar los valores únicos 'U' en el eje x y sus frecuencias 'P' en el eje y, con una escala logarítmica en el eje x
+plot(U, P, log = "x")
