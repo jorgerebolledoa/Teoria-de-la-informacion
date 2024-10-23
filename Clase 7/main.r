43892b875d086ec3c925676b0e0e1735b9178637
jorgerebolledoa <aziel.az18@gmail.com>
2024-10-23 16:44:20 -0300

    clase 7 update

--- Clase 7/main.r
+++ Clase 7/main.r
@@ -1,21 +1,19 @@
 pxy <- matrix(data = c(1/8,1/16,1/32,1/32,1/16,1/8,1/32,1/32,1/16,1/16,1/16,1/16,1/4,0,0,0), nrow = 4, ncol = 4, byrow = TRUE)
 px <- apply(pxy,MARGIN = 1, FUN = sum )
 py <- apply(pxy,MARGIN = 2, FUN =sum )
-print(px)
-print(py)
+
 H<- function(x) {
     x<-x[x>0]
     return(-sum(x * log2(x)))
 } 
-print(H(px))
-print(H(py))
-print(H(pxy))
-print((1/2)*H(pxy[,1]))
-print((1/4)*H(pxy[,2]))
-print((1/8)*H(pxy[,3]))
-print((1/8)*H(pxy[,4]))
-probY_x<-(1/4)*(H(pxy[1,]/py[1])+ (1/4)*H(pxy[2,]/py[1])+ (1/4)*H(pxy[3,]/py[1])+ (1/4)*H(pxy[4,]/py[1]))
-probX_y<-(1/2)*H(pxy[,1]/py[1])+ (1/4)*H(pxy[,2]/py[1])+ (1/8)*H(pxy[,3]/py[1])+ (1/8)*H(pxy[,4]/py[1])
-print(probX_y)
+probY_x <- py[1]* H(pxy[1,]/py[1])+py[2]* H(pxy[2,]/py[2])+ py[3]* H(pxy[3,]/py[3])+py[4]*H(pxy[4,]/py[4])
+probX_y <- px[1]*H(pxy[,1]/px)+px[2]* H(pxy[,2]/px)+ px[3]* H(pxy[,3]/px)+px[4]*H(pxy[,4]/px[4])
 print("Probabilidad de Y dado X")
 print(probY_x)
+print("Probabilidad de X dado Y")
+print(probX_y)
+
+for (i in 1:dim(pxy)[2]) {
+    selected<-pxy[i,]/py[i]
+    probY_x<-probY_x+py[i]*H(selected)
+}
