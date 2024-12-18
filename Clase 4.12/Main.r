0aca0db2e07dbc6bcd5d99eb58b228c9fd442243
jorgerebolledoa <aziel.az18@gmail.com>
2024-12-18 10:37:29 -0300

    4.12

--- Clase 4.12/Main.r
+++ Clase 4.12/Main.r
@@ -1,1 +1,15 @@
 Datos <- read.csv("past-rounded.csv", header = TRUE, sep = ",")
+npre13 <- Datos[as.integer(format(Datos[,"T"],"%T"))<2013,"N"]
+x <- sort(unique(npre13))
+px <- x * NA 
+plot(x, px)
+py <- y * NA
+dkl <- function(px, py) {
+    px <- px / sum(px)
+    py <- py / sum(py)
+    return(sum(px * log(px / py)))
+}
+print(dkl(px, py))
+print(dkl(py, px))
+
+
